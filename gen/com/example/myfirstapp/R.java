e5bb3b1d1f2435202986a9d3adbc54c5c594e003
zen6 <bilal.akabi@gmail.com>
2013-09-14 21:08:41 -0400

    added text boxes and function "pass gen" button

--- gen/com/example/myfirstapp/R.java
+++ gen/com/example/myfirstapp/R.java
@@ -25,9 +25,10 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_search=0x7f080002;
-        public static final int action_settings=0x7f080001;
-        public static final int edit_message=0x7f080000;
+        public static final int action_search=0x7f080003;
+        public static final int action_settings=0x7f080002;
+        public static final int editText1=0x7f080000;
+        public static final int editText2=0x7f080001;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
@@ -39,12 +40,14 @@ public final class R {
         public static final int main_activity_actions=0x7f070002;
     }
     public static final class string {
+        public static final int about=0x7f050007;
         public static final int action_search=0x7f050006;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int button_send=0x7f050003;
         public static final int edit_message=0x7f050002;
         public static final int hello_world=0x7f050005;
+        public static final int info=0x7f050008;
         public static final int title_activity_display_message=0x7f050004;
     }
     public static final class style {
--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.myfirstapp;
 
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Random;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,14 +11,15 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity 
 {
-	
+
 	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"; 
 	//define keys for intent extras using your app's package name as a prefix. Ensures uniqueness in App ecosystem.
-	
-	
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
@@ -35,11 +40,38 @@ public class MainActivity extends Activity
 	/**Called when the user clicks the Send button */
 	public void sendMessage (View view)
 	{
-		Intent intent = new Intent(this, DisplayMessageActivity.class);
-		EditText editText = (EditText) findViewById(R.id.edit_message);
-		String message = editText.getText().toString();
-		intent.putExtra(EXTRA_MESSAGE, message);
-		startActivity(intent);
+		//Intent intent = new Intent(this, DisplayMessageActivity.class);
+		//EditText editText = (EditText) findViewById(R.id.edit_message);
+
+		//Randomize Text
+		String message = Randomize(16);
+
+		//intent.putExtra(EXTRA_MESSAGE, message);
+		
+		TextView t = (TextView) findViewById(R.id.editText2);
+		t.setText(message);
+		//startActivity(intent);
+	}
+
+
+	private String Randomize(int size) 
+	{
+		// Make the symbol list!
+		String symbols = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()-=+[]{}"; //List is 53 elements long
+
+		// Initialize the randomized string
+		String output = "";
+
+		// Initialize SecureRandom
+		SecureRandom rnd = new SecureRandom();
+
+		for (int i = 0; i < size; i++)
+		{
+				output += symbols.charAt(rnd.nextInt(53));
+		}
+
+		return output;
+
 	}
 
 }
